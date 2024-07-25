67f0a58141806fbe47388ab0778f16a71a73eb59
Yoni190 <yfisuhu@gmail.com>
2024-07-25 14:31:25 +0300

    Add function to play a round of Rock, Paper, and Scissors

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
+let humanScore = 0, computerScore = 0;
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3) + 1;
     if(choice == 1){
-        return "Rock";
+        return "rock";
     }
     else if(choice == 2){
-        return "Paper";
+        return "paper";
     }
     else{
-        return "Scissors";
+        return "scissors";
     }
 }
+
 function getHumanChoice(){
     let choice = prompt("Enter Rock, Paper, or Scissors: ");
     choice = choice.toLowerCase();
     return choice;
 }
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice){
+    if((humanChoice == "paper" && computerChoice == "paper") || (humanChoice == "rock" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "scissors")){
+        console.log("It's a tie");
+    }
+    else if(humanChoice == "paper" && computerChoice == "rock"){
+        console.log("You win! Paper beats rock");
+        humanScore++;
+    }
+    else if(humanChoice == "rock" && computerChoice == "scissors"){
+        console.log("You win! Rock beats scissors");
+        humanScore++;
+    }
+    else if(humanChoice == "scissors" && computerChoice == "paper"){
+        console.log("You win! Scissors beats paper");
+        humanScore++;
+    }
+    else if(humanChoice == "rock" && computerChoice == "paper"){
+        console.log("You lose! Paper beats rock");
+        computerScore++;
+    }
+    else if(humanChoice == "scissors" && computerChoice == "rock"){
+        console.log("You lose! Rock beats scissors");
+        computerScore++;
+    }
+    else if(humanChoice == "paper" && computerChoice == "scissors"){
+        console.log("You lose! Scissors beats paper");
+        computerScore++;
+    }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+playRound(humanSelection, computerSelection);
